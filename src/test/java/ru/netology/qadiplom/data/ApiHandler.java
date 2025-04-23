771497a083326c029be714ed99ac277ba688bee1
dmitry-mak <akkaru@gmail.com>
2025-04-23 19:54:45 +0300

    Refactor ApiHandler class in accordance to DRY principles

--- src/test/java/ru/netology/qadiplom/data/ApiHandler.java
+++ src/test/java/ru/netology/qadiplom/data/ApiHandler.java
@@ -9,20 +9,25 @@ import static io.restassured.RestAssured.given;
 
 public class ApiHandler {
 
+    private static final String BASE_URI = "http://localhost";
+    private static final String PAYMENT = "/api/v1/pay";
+    private static final String CREDIT = "/api/v1/credit";
+
     public static RequestSpecification specification = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
+            .setBaseUri(BASE_URI)
             .setPort(8080)
             .setAccept(ContentType.JSON)
             .setContentType(ContentType.JSON)
             .log(LogDetail.ALL)
             .build();
 
+
     public static String sendValidRequest() {
         String statusRequest = given()
                 .spec(specification)
                 .body(DataHandler.getApprovedCard())
                 .when()
-                .post("/api/v1/pay")
+                .post(PAYMENT)
                 .then()
                 .statusCode(200)
                 .extract()
@@ -36,7 +41,7 @@ public class ApiHandler {
                 .spec(specification)
                 .body(DataHandler.getDeclinedCard())
                 .when()
-                .post("/api/v1/pay")
+                .post(PAYMENT)
                 .then()
                 .statusCode(200)
                 .extract()
@@ -50,15 +55,58 @@ public class ApiHandler {
                 .spec(specification)
                 .body(DataHandler.getUnregisteredCard())
                 .when()
-                .post("/api/v1/pay")
+                .post(PAYMENT)
                 .then()
                 .statusCode(500)
                 .extract()
 //                .response()
 //                .asString();
-                .path("message");
+                .path("status").toString();
 
         return statusRequest;
     }
 
+
+    public static String sendRequest(String endpoint, Object cardInfo, int statusCode) {
+        var status = given()
+                .spec(specification)
+                .body(cardInfo)
+                .when()
+                .post(endpoint)
+                .then()
+                .statusCode(statusCode)
+                .extract()
+                .path("status");
+        return String.valueOf(status);
+    }
+
+    public static String sendRequestValidApprovedPayment() {
+        String result = sendRequest(PAYMENT, DataHandler.getApprovedCard(), 200);
+        return result;
+    }
+
+    public static String sendRequestValidDeclinedPayment() {
+        String result = sendRequest(PAYMENT, DataHandler.getDeclinedCard(), 200);
+        return result;
+    }
+
+    public static String sendRequestUnregisteredPayment() {
+        String result = sendRequest(PAYMENT, DataHandler.getUnregisteredCard(), 500);
+        return result;
+    }
+
+    public static String sendRequestValidApprovedCredit() {
+        String result = sendRequest(CREDIT, DataHandler.getApprovedCard(), 200);
+        return result;
+    }
+
+    public static String sendRequestValidDeclinedCredit() {
+        String result = sendRequest(CREDIT, DataHandler.getDeclinedCard(), 200);
+        return result;
+    }
+
+    public static String sendRequestUnregisteredCredit() {
+        String result = sendRequest(CREDIT, DataHandler.getUnregisteredCard(), 500);
+        return result;
+    }
 }
--- src/test/java/ru/netology/qadiplom/test/ApiTest.java
+++ src/test/java/ru/netology/qadiplom/test/ApiTest.java
@@ -6,18 +6,49 @@ import ru.netology.qadiplom.data.ApiHandler;
 
 public class ApiTest {
 
+//    @Test
+//    public void testApiApprovedCard() {
+//        Assertions.assertEquals("APPROVED", ApiHandler.sendValidRequest());
+//    }
+//
+//    @Test
+//    public void testApiDeclinedCard() {
+//        Assertions.assertEquals("DECLINED", ApiHandler.sendValidRequestDeclinedCard());
+//    }
+//
+//    @Test
+//    public void testApiUnregisteredCard() {
+//        Assertions.assertEquals("500", ApiHandler.sendValidRequestUnregisteredCard());
+//    }
+
     @Test
-    public void testApiApprovedCard() {
-        Assertions.assertEquals("APPROVED", ApiHandler.sendValidRequest());
+    public void shouldSendValidRequestApprovedPayment() {
+        Assertions.assertEquals("APPROVED", ApiHandler.sendRequestValidApprovedPayment());
     }
 
     @Test
-    public void testApiDeclinedCard() {
-        Assertions.assertEquals("DECLINED", ApiHandler.sendValidRequestDeclinedCard());
+    public void shouldSendValidRequestDeclinedPayment() {
+        Assertions.assertEquals("DECLINED", ApiHandler.sendRequestValidDeclinedPayment());
     }
 
     @Test
-    public void testApiUnregisteredCard() {
-        Assertions.assertEquals("400 Bad Request", ApiHandler.sendValidRequestUnregisteredCard());
+    public void shouldSendRequestUnregisteredPayment() {
+        Assertions.assertEquals("500", ApiHandler.sendRequestUnregisteredPayment());
     }
+
+    @Test
+    public void shouldSendValidRequestApprovedCredit() {
+        Assertions.assertEquals("APPROVED", ApiHandler.sendRequestValidApprovedCredit());
+    }
+
+    @Test
+    public void shouldSendValidRequestDeclinedCredit() {
+        Assertions.assertEquals("DECLINED", ApiHandler.sendRequestValidDeclinedCredit());
+    }
+
+    @Test
+    public void shouldSendRequestUnregisteredCredit() {
+        Assertions.assertEquals("500", ApiHandler.sendRequestUnregisteredCredit());
+    }
+
 }
